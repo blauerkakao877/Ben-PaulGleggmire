a05d9b404d3c1ba7a5378f4c197970dc04fb24da
blauerkakao877 <131390374+blauerkakao877@users.noreply.github.com>
2025-07-12 15:47:58 +0200

    Add files via upload

--- FE_2025/Autostart/autostart.sh
+++ FE_2025/Autostart/autostart.sh
@@ -5,6 +5,7 @@ date
 cd /home/pi/FE_2025/Work
 source /home/pi/env/bin/activate
 # python3 Openrace_kamera.py
-# python3 Obstaclerace_kamera_u-turn_3.py
-# python3 Obstaclerace_kamera_2025new.py
-  python3 Obstaclerace_kamera_Regio25.py
+# python3 Openrace_kamera_prox.py
+# python3 Obstaclerace_kamera_Regio25.py
+# python3 Obstaclerace_kamera_Regio25_final.py
+ python3 Obstaclerace_kamera_inter.py
