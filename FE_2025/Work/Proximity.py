665c2e8a60f18d94e4267775ba0179ab9a4a7dc0
blauerkakao877 <131390374+blauerkakao877@users.noreply.github.com>
2025-07-19 14:16:35 +0200

    New Prox sensors

--- FE_2025/Work/Proximity.py
+++ FE_2025/Work/Proximity.py
@@ -1,46 +1,50 @@
 # Bibliotheken einbinden
 import RPi.GPIO as GPIO
 import time
+import LED as L
 
 # GPIO Modus (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
 
 # GPIO Pins zuweisen
 #Rechts vorne
-GPIO_RV = 4
-GPIO_LV = 17
-GPIO_V = 12
+GPIO_HL = 5
+GPIO_VL = 6
+#GPIO_V = 12
 
-GPIO.setup(GPIO_RV, GPIO.IN)
-GPIO.setup(GPIO_LV, GPIO.IN)
-GPIO.setup(GPIO_V, GPIO.IN)
-
+GPIO.setup(GPIO_HL, GPIO.IN)
+GPIO.setup(GPIO_VL, GPIO.IN)
+#GPIO.setup(GPIO_V, GPIO.IN)
 
 def prox_alarm():
 
-    alarm_RV = False
-    alarm_LV = False
-    alarm_V = False
-    wertR = GPIO.input(GPIO_RV)
-    wertL = GPIO.input(GPIO_LV)
+    alarm_HL = False
+    alarm_VL = False
+    #alarm_V = False
+    wertHL = GPIO.input(GPIO_HL)
+    wertVL = GPIO.input(GPIO_VL)
     #wertM = GPIO.input(GPIO_V)
     
-    #if wertR == 0:
-        #alarm_RV = True
+    if wertHL == 0:
+        alarm_HL = True
+        L.led_W21()
+    else:
+        alarm_HL = False
+        L.led_W20()
         
-    #if wertL == 0:
-        #alarm_LV = True
+    if wertVL == 0:
+        alarm_VL = True
         
     #if wertM == 0:
         #alarm_V = True
         
-    return alarm_RV, alarm_LV, alarm_V
+    return alarm_HL, alarm_VL
 
 if __name__ == '__main__':
     try:
         while True:
-            alarm_RV, alarm_LV, alarm_V = prox_alarm()
-            print("Hindernis rechts-links-vorne: ", alarm_RV, alarm_LV, alarm_V)
+            alarm_HL, alarm_VL = prox_alarm()
+            print("Hindernis HL-VL: ", alarm_HL, alarm_VL,)
             time.sleep(0.1)
 
     # Beim Abbruch durch STRG+C resetten
---
+++ FE_2025/Work/VL_sensors.py
@@ -0,0 +1,90 @@
+import board
+import busio
+import time
+from adafruit_extended_bus import ExtendedI2C as I2C
+import adafruit_tca9548a
+import adafruit_vl53l4cd
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_HL = 6
+GPIO_VL = 5
+
+GPIO.setup(GPIO_HL, GPIO.OUT)
+GPIO.setup(GPIO_VL, GPIO.OUT)
+
+# software i2c bus 9 on sda=17, scl=27
+i2c = I2C(9)
+#i2c = board.I2C()
+
+#multiplexer object
+tca = adafruit_tca9548a.TCA9548A(i2c)
+
+#sensor an i2c slot 0
+sensorVL = adafruit_vl53l4cd.VL53L4CD(tca[0])
+sensorVL.inter_measurement = 0 # no wait between measurements
+sensorVL.timing_budget = 20 # 20 msec ranging time
+
+#sensor an i2c slot 3
+sensorHL = adafruit_vl53l4cd.VL53L4CD(tca[6])
+sensorHL.inter_measurement = 0
+sensorHL.timing_budget = 20
+
+print("VL53L4CD Simple Test.")
+print("--------------------")
+model_id, module_type = sensorVL.model_info
+print("Model ID: 0x{:0X}".format(model_id))
+print("Module Type: 0x{:0X}".format(module_type))
+print("Timing Budget: {}".format(sensorVL.timing_budget))
+print("Inter-Measurement: {}".format(sensorVL.inter_measurement))
+print("--------------------")
+
+time.sleep(2.0)
+
+count_VL = 0
+count_HL = 0
+
+sensorVL.start_ranging()
+sensorHL.start_ranging()
+
+startTime = time.time()
+
+
+while True:
+    try:
+        if not sensorVL.data_ready:
+            pass
+        else:
+            sensorVL.clear_interrupt()
+            print("Distance VL: {} cm".format(sensorVL.distance))
+            #count_VL = count_VL + 1
+    except:
+        print("error VL")
+    
+    try:
+        if not sensorHL.data_ready:
+            pass
+        else:
+            sensorHL.clear_interrupt()
+            print("Distance HL: {} cm".format(sensorHL.distance))
+            
+            dist_HL = sensorHL.distance
+            #count_HL = count_HL + 1
+            if dist_HL < 5.0:
+                GPIO.output(GPIO_HL, True)
+            else:
+                GPIO.output(GPIO_HL, False)
+    except:
+        print("error HL")
+        
+        
+        
+    """if time.time() > startTime + 10.0:
+        print("Count VL: {} ".format(count_VL))
+        print("Count HL: {} ".format(count_HL))
+        time.sleep(2.0)
+        count_VL = 0
+        count_HL = 0
+        startTime = time.time()"""
