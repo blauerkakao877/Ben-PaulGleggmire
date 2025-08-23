b598f40750be54db9b44212115770248ddfaed80
blauerkakao877 <131390374+blauerkakao877@users.noreply.github.com>
2025-08-23 16:59:37 +0200

    Add files via upload

---
+++ FE_2025/Work/autostart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Roadrunner Start um: "
+date
+cd /home/pi/FE_2025/Work
+source /home/pi/env/bin/activate
+# python3 Openrace_kamera.py
+# python3 Obstaclerace_kamera_inter.py
+ python3 einparken_ausparken_TEST.py
