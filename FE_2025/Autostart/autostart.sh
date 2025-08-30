4b615b22f3e29c47ca9b9809d6ff0c74a571dcf9
blauerkakao877 <131390374+blauerkakao877@users.noreply.github.com>
2025-08-30 16:34:36 +0200

    Add files via upload

--- FE_2025/Autostart/autostart.sh
+++ FE_2025/Autostart/autostart.sh
@@ -5,7 +5,5 @@ date
 cd /home/pi/FE_2025/Work
 source /home/pi/env/bin/activate
 # python3 Openrace_kamera.py
-# python3 Openrace_kamera_prox.py
-# python3 Obstaclerace_kamera_Regio25.py
-# python3 Obstaclerace_kamera_Regio25_final.py
- python3 Obstaclerace_kamera_inter.py
+# python3 Obstaclerace_kamera_inter.py
+python3 Obstaclerace_kamera_inter_paral.py
---
+++ FE_2025/Autostart/autostart_prox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Prox Start um: "
+date
+cd /home/pi/FE_2025/Work
+source /home/pi/env/bin/activate
+
+ python3 VL_sensors.py
